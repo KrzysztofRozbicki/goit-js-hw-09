9fc85983f2212bc6f074395930d6cf0b6a6e524d
KrzysztofRozbicki <104444811+KrzysztofRozbicki@users.noreply.github.com>
2023-03-15 14:41:05 +0000

    Deploying to gh-pages from @ KrzysztofRozbicki/goit-js-hw-09@5d6000727979b6a4e92f9824086a6fad880b99c5 🚀

---
+++ 01-color-switcher.f8f985b9.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;document.getElementById;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.f8f985b9.js.map
